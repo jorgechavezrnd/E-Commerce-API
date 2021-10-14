abbb1bfd9ba8b0e064b1b79b387fa70c6446164a
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-10-14 08:19:54 -0400

    Initial commit. Adding API REST example in momery

---
+++ ECommerceAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,89 @@
+﻿using ECommerceAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ECommerceAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly List<Category> _categories;
+
+        public CategoriesController(List<Category> categories)
+        {
+            _categories = categories;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            return Ok(_categories);
+        }
+
+        [HttpPost]
+        public IActionResult PostCategories([FromBody] CategoryRequest request)
+        {
+            var category = new Category
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = request.Name,
+                Description = request.Description
+            };
+
+            _categories.Add(category);
+            return Ok(new
+            {
+                Id = category.Id
+            });
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult PutCategories(string id, [FromBody] CategoryRequest request)
+        {
+            var find = _categories.FirstOrDefault(p => p.Id == id);
+
+            if (find == null)
+            {
+                return NotFound(new
+                {
+                    id
+                });
+            }
+
+            find.Description = request.Description;
+            find.Name = request.Name;
+
+            return Ok(new
+            {
+                id
+            });
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteCategories(string id)
+        {
+            var find = _categories.FirstOrDefault(p => p.Id == id);
+
+            if (find == null)
+            {
+                return NotFound(new
+                {
+                    id
+                });
+            }
+
+            _categories.Remove(find);
+
+            return Ok(new
+            {
+                id
+            });
+        }
+
+    }
+}
---
+++ ECommerceAPI/Model/CategoryRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerceAPI.Model
+{
+    public class CategoryRequest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
