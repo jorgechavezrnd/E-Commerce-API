511f1dc653edfacfc449d1a9aa0766a3dfde8163
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-16 19:35:33 -0400

    Add settings files for development and production

--- ECommerceAPI/Startup.cs
+++ ECommerceAPI/Startup.cs
@@ -6,6 +6,7 @@ using ECommerceAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -17,6 +18,13 @@ namespace ECommerceAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -31,7 +39,7 @@ namespace ECommerceAPI
 
             services.AddDbContext<ECommerceDbContext>(options =>
             {
-                options.UseSqlServer("Server=localhost;Database=EcommerceDb;Integrated Security=true");
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
                 options.LogTo(Console.WriteLine, LogLevel.Information);
             });
 
@@ -54,6 +62,11 @@ namespace ECommerceAPI
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerceAPI v1"));
+            }
+            else
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("../swagger/v1/swagger.json", "ECommerceAPI v1"));
             }
 
             app.UseRouting();
