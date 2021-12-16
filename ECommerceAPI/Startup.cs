a3eb0eb001b1b507ae25a0c30b8273932e448871
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-16 18:37:34 -0400

    Add packages for Swagger and base configuration

--- ECommerceAPI/Startup.cs
+++ ECommerceAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 
@@ -33,6 +34,16 @@ namespace ECommerceAPI
                 options.UseSqlServer("Server=localhost;Database=EcommerceDb;Integrated Security=true");
                 options.LogTo(Console.WriteLine, LogLevel.Information);
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ECommerce API",
+                    Version = "v1",
+                    Description = "API para el curso FullStack"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +52,8 @@ namespace ECommerceAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECommerceAPI v1"));
             }
 
             app.UseRouting();
