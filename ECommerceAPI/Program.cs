d52fef2a3e581551ab9111b91b51ab7b6842470a
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-01-05 22:09:21 -0400

    Remove unnecessary using statements in Program.cs

--- ECommerceAPI/Program.cs
+++ ECommerceAPI/Program.cs
@@ -6,9 +6,6 @@ using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.MSSqlServer;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ECommerceAPI
 {
