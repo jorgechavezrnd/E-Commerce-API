cb70202884d788e6091dc120d58d0649c1532abf
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-01-05 22:07:55 -0400

    Add Serilog for logs and use it

--- ECommerceAPI/Program.cs
+++ ECommerceAPI/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +21,21 @@ namespace ECommerceAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(x =>
+                {
+                    x.AddConsole();
+                })
+                .UseSerilog((options, logging) =>
+                {
+                    logging.WriteTo.Console();
+                    logging.WriteTo.MSSqlServer(options.Configuration.GetConnectionString("Default"),
+                        new MSSqlServerSinkOptions
+                        {
+                            AutoCreateSqlTable = true,
+                            TableName = "ApiLogs",
+                            BatchPeriod = TimeSpan.FromDays(1)
+                        }, restrictedToMinimumLevel: LogEventLevel.Warning);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
